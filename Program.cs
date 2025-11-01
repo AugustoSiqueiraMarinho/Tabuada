54d6a7d70c34389058df282cb5c08e49dbe893a7
Augusto S <augustosiqueira000@gmail.com>
2025-10-31 21:23:28 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("--- Tabuada ---");
+ 
+int n = 1;
+int x = 3; // Altere para que o usuario forneça o número
+ 
+while (n <= 10)
+{
+    Console.WriteLine($"{x} x {n} = {x*n}");
+    n = n + 1;
+}
+ 
